108a751487a31aa590b95924dfe14cdc6aecd3e0
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-07-06 16:40:20 +0500

    Dispatch Your first Job

--- Techniques/Queues/routes/web.php
+++ Techniques/Queues/routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//  logger("Hello World"); # It executes, so we can view it in laravel.log file
+
+  # We use redis for queue so install it and enable it in php. so when i run php artisan queue:work then it executes and show in the log file, and we can also set delays
+  dispatch(function () {
+    logger("I have to tell you about Delay Queue Job");
+  })->delay(now()->addMinutes(1));
+
+  return "Finished";
 });
