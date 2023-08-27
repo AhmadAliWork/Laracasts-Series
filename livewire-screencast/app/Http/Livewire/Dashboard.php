00c6b4652faf5c4d9be5547d390698f7b8802d89
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-08-27 23:01:26 +0500

    sorting

--- livewire-screencast/app/Http/Livewire/Dashboard.php
+++ livewire-screencast/app/Http/Livewire/Dashboard.php
@@ -11,10 +11,23 @@ class Dashboard extends Component
     use WithPagination;
 
     public string $search = '';
+    public string $sortDirection = 'asc';
+    public string $sortField = 'title';
+
+    protected $queryString = ["sortField", "sortDirection"];
+
+    public function sortBy($field)
+    {
+        $this->sortDirection = $this->sortField === $field
+          ? $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc'
+          : 'asc';
+        $this->sortField = $field;
+    }
+
     public function render()
     {
         return view('livewire.dashboard', [
-          'transactions' => Transaction::search('title', $this->search)->paginate(10)
+          'transactions' => Transaction::search('title', $this->search)->orderBy($this->sortField, $this->sortDirection)->paginate(10)
         ]);
     }
 }
